6b44f0620fb8db598dd3b99ce4a0945f5f65782e
jbpark <jbeompark.bill@gmail.com>
2022-04-03 18:29:13 +0900

    initial

---
+++ Calculator2.py
@@ -0,0 +1,15 @@
+class Calc():
+    totalSum = 0
+    def __init__(self) -> None:
+        self.totalSum = 0
+    def sum(self,a,b):
+        self.totalSum = a+b
+        print("여기의합산은?===>"+str(self.totalSum))
+        return a+b
+    
+if __name__ == "__main__" :
+    temCalc = Calc()
+    returnVal = temCalc.sum(5,7)
+    print("returnVal===>"+str(returnVal))
+    print("returnVal===>"+str(returnVal))
+    print("returnVal===>"+str(returnVal))
